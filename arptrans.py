588d8e0d15bccd43f4a34534b890e9dd41c5cf60
Thomas Verweij <thomasverweij01@gmail.com>
2017-11-17 16:37:40 +0100

    more updatess

--- arptrans.py
+++ arptrans.py
@@ -1,14 +1,24 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
 ip = input("IP: ")
-s = input("payload: ")
 
-h = "".join(["{:02x}".format(ord(c)) for c in s])
-h = [h[i:i+2] for i in range(0,len(h),2)]
-h = [h[i:i+6] for i in range(0,len(h),6)]  
-h[-1].extend(['65'] * (6 - len(h[-1])))
-macs = [":".join(a) for a in h]
-print(macs)
+def packetgen(s):
+    h = "".join(["{:02x}".format(ord(c)) for c in s])
+    h = [h[i:i+2] for i in range(0,len(h),2)]
+    h = [h[i:i+6] for i in range(0,len(h),6)]  
+    h[-1].extend(['20'] * (6 - len(h[-1])))
+    macs = [":".join(a) for a in h]
+    return ARP(hwsrc=macs, pdst=ip, psrc='192.168.178.188')
 
-send(ARP(hwsrc=macs, psrc=ip), inter=1)
-send(ARP(hwsrc="eeeeeeeeeeee", psrc=ip), count=3)
+exit = False    
+
+while not exit:
+    ui = input("payload: ")
+    if ui == 'exit':
+        exit = True
+    else:
+        send(packetgen(ui))
+        send(ARP(hwsrc="ee:ee:ee:ee:ee:ee", pdst=ip, psrc='192.168.178.188'), count=3)
+
